b8ecad6d0fe6c3cc94506711f9f499a109e5bfad
Lavan Sumanan <lavan.sumanan@gmail.com>
2022-02-05 17:31:20 -0500

    added routes for frontend

---
+++ client/src/routes/Home.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ client/src/routes/SignIn.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const SignIn = () => {
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+    </div>
+  );
+};
+
+export default SignIn;
