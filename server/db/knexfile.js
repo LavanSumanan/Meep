16f83b420a4fd45f3e2ee5f9ef88c5e453d0b5c6
Lavan Sumanan <lavan.sumanan@gmail.com>
2022-02-06 01:54:04 -0500

    added knex

---
+++ server/db/knexfile.js
@@ -0,0 +1,24 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'DATABASE NAME HERE',
+      user:     'USERNAME HERE',
+      password: 'PASSWORD HERE'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+};
---
+++ server/db/migrations/20220206055006_init.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('user', table => {
+    table.increments('id');
+    table.string('first_name').notNullable();
+    table.string('last_name').notNullable();
+    table.string('email').notNullable().unique();
+    table.int('age').notNullable();
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('user');
+};
